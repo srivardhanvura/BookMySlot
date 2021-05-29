f349230f16c366595535273a266a2399f05ff354
Srivardhan <srivardhanvura@gmail.com>
2021-05-29 19:10:11 +0530

    image slider changed

--- app/src/main/java/com/itt/bookmyslot/Fragment/HomeFragment.java
+++ app/src/main/java/com/itt/bookmyslot/Fragment/HomeFragment.java
@@ -2,7 +2,6 @@ package com.itt.bookmyslot.Fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +9,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
 
-import com.itt.bookmyslot.Adapter.ImageAdapter;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.itt.bookmyslot.R;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -22,17 +24,22 @@ public class HomeFragment extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        mContext=context;
+        mContext = context;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        ViewPager viewPager=view.findViewById(R.id.view_pager);
-        ImageAdapter adapter=new ImageAdapter(mContext);
-        viewPager.setAdapter(adapter);
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+
+        imageList.add(new SlideModel(R.drawable.mit2, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.mit1, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.mit3, ScaleTypes.CENTER_CROP));
+
+        ImageSlider imageSlider = view.findViewById(R.id.slider);
+        imageSlider.setImageList(imageList);
 
         return view;
     }
